74650583dea4617710afc60dbca3ed4170fd3ec5
Andrea <andrecogno@hotmail.it>
2021-04-19 13:32:17 +0000

    Add xgboost baseline model for all targets

---
+++ andrea/recsys-2021/src/scoring.py
@@ -0,0 +1,17 @@
+from sklearn.metrics import average_precision_score, log_loss
+
+def calculate_ctr(gt):
+    positive = len([x for x in gt if x == 1])
+    ctr = positive/float(len(gt))
+    return ctr
+
+def relative_cross_entropy_score(gt, pred):
+    cross_entropy = log_loss(gt, pred)
+    data_ctr = calculate_ctr(gt)
+    strawman_cross_entropy = log_loss(gt, [data_ctr for _ in range(len(gt))])
+    return (1.0 - cross_entropy/strawman_cross_entropy)*100.0
+
+def compute_score(y_true, y_score):
+    ap = average_precision_score(y_true, y_score)
+    rce = relative_cross_entropy_score(y_true, y_score)
+    return ap, rce
