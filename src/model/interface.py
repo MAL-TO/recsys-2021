01d7a00f0da7b6ecaaa6f9e5191c1d780bca0c3b
Andrea Cognolato <andrecogno@hotmail.it>
2021-04-27 20:25:20 +0200

    Add run script and inference.py for submission Create serialized_models folder Add load_pretrained to ModelInterface

--- src/model/interface.py
+++ src/model/interface.py
@@ -9,27 +9,34 @@ class ModelInterface(metaclass=abc.ABCMeta):
                 callable(subclass.predict) and
                 hasattr(subclass, 'evaluate') and
                 callable(subclass.evaluate) and
+                hasattr(subclass, 'load_pretrained') and
+                callable(subclass.load_pretrained) and
                 hasattr(subclass, 'save_to_logs') and
                 callable(subclass.save_to_logs) or
                 NotImplemented)
-        
+
     @abc.abstractmethod
     def fit(self, train_data, valid_data, hyperparams):
         """Fit model to given training data and validate it.
         Returns the best model found in validation."""
         raise NotImplementedError
-    
+
     @abc.abstractmethod
     def predict(self, test_data):
         """Predict test data. Returns predictions."""
         raise NotImplementedError
-        
+
     @abc.abstractmethod
     def evaluate(self, test_data):
         """Predict test data and evaluate the model on metrics.
         Returns the metrics."""
         raise NotImplementedError
-    
+
+    @abc.abstractmethod
+    def load_pretrained(self):
+        """Load a pretrained model saved in `models/`."""
+        raise NotImplementedError
+
     @abc.abstractmethod
     def save_to_logs(self):
         """Save the results of the latest test performed to logs."""
--- src/model/native_xgboost_baseline.py
+++ src/model/native_xgboost_baseline.py
@@ -6,11 +6,14 @@ import pandas as pd
 
 from metrics import compute_score
 
+from pathlib import Path
+from constants import ROOT_DIR
+
 class Model(ModelInterface):
     def __init__(self):
         self.models = {}
         self.target_columns = ["reply", "retweet", "retweet_with_comment", "like"]
-        
+
         # TODO(Andrea): is there a better way to have this? Instead of a single
         # config.json have each model tell us which self.features to use
         self.features = [
@@ -20,12 +23,17 @@ class Model(ModelInterface):
             "engaging_user_following_count",
         ]
 
+    @staticmethod
+    def serialized_model_path_for_target(target: str) -> str:
+        p = Path(ROOT_DIR) / '../serialized_models' / f'native_xgboost_basline_{target}.model'
+        return p.resolve()
+
     def fit(self, train_ksdf, valid_ksdf, _hyperparameters):
         ###############################################################################
         # Convert to koalas dataframes to pandas
         train_df = train_ksdf.to_pandas()
         valid_df = valid_ksdf.to_pandas()
-        
+
         ###############################################################################
         # Convert timestamp labels to binary labels (TODO(Andrea): this should be done
         # on import imho)
@@ -35,40 +43,42 @@ class Model(ModelInterface):
             "retweet_with_comment_timestamp",
             "like_timestamp",
         ]
-        
+
         for df in [train_df, valid_df]:
             for old_col, new_col in zip(old_target_columns, self.target_columns):
                     df[new_col] = df[old_col].notnull()
-        
+
         ###############################################################################
         # Train and save models
         xgb_parameters = {"objective": "binary:logistic", "eval_metric": "logloss"}
         for target in self.target_columns:
             dtrain = xgb.DMatrix(data=train_df[self.features], label=train_df[target])
             model = xgb.train(xgb_parameters, dtrain=dtrain)
+            model.save_model(self.serialized_model_path_for_target(target))
             self.models[target] = model
 
-    def predict(self, test_ksdf):
+
+    def predict(self, test_ksdf) -> pd.DataFrame:
         ###############################################################################
         # Convert to koalas dataframes to pandas
         test_df = test_ksdf.to_pandas()
-        
+
         ###############################################################################
         # Compute predictions for all models
         dtest = xgb.DMatrix(data=test_df[self.features])
         predictions_df = pd.DataFrame()
         for target in self.target_columns:
             predictions_df[target] = self.models[target].predict(dtest)
-        
+
         return predictions_df
-    
+
     def evaluate(self, test_ksdf, save_to_logs=False):
         ###############################################################################
         # Convert to koalas dataframes to pandas
         # TODO(Andrea): since we have fit and predict we do we need evaluate to
         # be implemented in the model?
         predictions_df = self.predict(test_ksdf)
-    
+
         # Convert timestamp labels to binary labels (TODO(Andrea): this should be done
         # on import imho)
         old_target_columns = [
@@ -83,7 +93,7 @@ class Model(ModelInterface):
                     df[new_col] = df[old_col].notnull()
 
         ###############################################################################
-        # Compute metrics for all models 
+        # Compute metrics for all models
         results = {}
         for column in predictions_df.columns:
             AP, RCE = compute_score(test_ksdf[column].to_numpy(), predictions_df[column].to_numpy())
@@ -91,6 +101,12 @@ class Model(ModelInterface):
             results[f'{column}_RCE'] = RCE
 
         return results
+
+    def load_pretrained(self):
+        for target in self.target_columns:
+            model = xgb.Booster()
+            model.load_model(self.serialized_model_path_for_target(target))
+            self.models[target] = model
 
     def save_to_logs(self, metrics):
         pass
