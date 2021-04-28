ac452807f620af117f0a0953a5ddca01d73fdbb8
Andrea Cognolato <andrecogno@hotmail.it>
2021-04-28 16:37:13 +0200

    Add binarize_timestamps target feature

---
+++ src/preprocessor/targets/binarize_timestamps.py
@@ -0,0 +1,8 @@
+def binarize_timestamps(raw_data, features):
+    return {
+        "reply": raw_data["reply_timestamp"].notna(),
+        "retweet": raw_data["retweet_timestamp"].notna(),
+        "retweet_with_comment": raw_data["retweet_with_comment_timestamp"].notna(),
+        "like": raw_data["like_timestamp"].notna()
+    }
+
