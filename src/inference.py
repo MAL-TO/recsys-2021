e13995a94692ccd4101a80a1869dd4ddd950a92d
Andrea Cognolato <andrecogno@hotmail.it>
2021-05-01 20:39:03 +0200

    Update inference.py

--- src/inference.py
+++ src/inference.py
@@ -10,48 +10,26 @@ from constants import ROOT_DIR
 from data.importer import import_data
 from data.splitter import train_valid_test_split_bounds
 
-from preprocessor.config import FeatureConfig
-from preprocessor.features import extract_features
+from preprocessor.features_store import FeatureStore
 
-# TODO(Andrea): should this depend on a command line argument?
-from model.native_xgboost_baseline import Model
+from model.h2o_xgboost_baseline import Model
 
-# Files and paths
-RAW_DATA_INPUT_PATH = {
-    "cluster_complete": "hdfs://BigDataHA/user/s277309/recsys_data/",
-    "cluster_sample_one_hund": "hdfs://BigDataHA/user/s277309/recsys_data_sample/one_hund/",
-    "cluster_sample_200k": "hdfs://BigDataHA/user/s277309/recsys_data_sample/local/sample200k",
-    "local_sampled": "../data/raw/sample_0.0134_noid_notext_notimestamp.parquet",
-    "local_sampled_small": os.path.join(ROOT_DIR, "../data/raw/sample200k"),
-    "test": os.path.join(ROOT_DIR, "../test"),
-}
+PATH_PREPROCESSED = os.path.join(ROOT_DIR, '../data/preprocessed')
 
-FEATURE_CONFIG_FILE = os.path.join(ROOT_DIR, "preprocessor", "config.json")
-
-
-def main(dataset_name):
-    feature_config = FeatureConfig(FEATURE_CONFIG_FILE)
-
-    print("Importing data")
-    raw_data = import_data(dataset_name,
-                           path=RAW_DATA_INPUT_PATH[dataset_name])
-
-    # TODO [feature store]: only extract features that do not
-    # already exist in data/preprocessed
-
-    print("Extracting features")
-    extracted_features = extract_features(raw_data, feature_config)
-
-    # Drop raw_data features not enabled in config.json
-    enabled_default_features = feature_config.get_enabled_default_features_list()
-    raw_data = raw_data[enabled_default_features]
-
-    # Merge all into a single dataframe with index
-    features_union_df = ks.concat([raw_data] + list(extracted_features.values()), axis=1)
-
-    print("Loading model")
+def main():
+    print("Initializing model...", end = " ")
     model = Model()
     model.load_pretrained()
+    print("Done")
+
+    print("Importing data...", end=" ")
+    raw_data = import_data(os.path.join(ROOT_DIR, '../test'))
+    print("Done")
+
+    print("Assembling dataset...")
+    store = FeatureStore(PATH_PREPROCESSED, model.enabled_features, raw_data, is_cluster=False)
+    features_union_df = store.get_dataset()
+    print("Dataset ready")
 
     print('Computing predictions')
     predictions_df = model.predict(features_union_df)
@@ -59,12 +37,12 @@ def main(dataset_name):
     assert len(features_union_df) == len(predictions_df), \
         "features and predictions must have the same length"
 
-    predictions_df['tweet_id'] = features_union_df['tweet_id'].to_numpy()
-    predictions_df['user_id'] = features_union_df['engaging_user_id'].to_numpy()
+    predictions_df['tweet_id'] = raw_data['tweet_id'].to_numpy()
+    predictions_df['user_id'] = raw_data['engaging_user_id'].to_numpy()
 
     predictions_df[
         ['tweet_id', 'user_id', 'reply', 'retweet', 'retweet_with_comment', 'like']
     ].to_csv('results.csv', mode='a', header=False, index=False)
 
 if __name__ == "__main__":
-    main("test")
+    main()
