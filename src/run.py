2167a6f78410f777575be43ca0adfd4acdd84cf7
Andrea Cognolato <andrecogno@hotmail.it>
2021-05-02 16:08:20 +0200

    Add dataset path, model, and is_cluster command line arguments Closes #13

--- src/run.py
+++ src/run.py
@@ -3,52 +3,39 @@ import os
 
 import pandas as pd
 import numpy as np
-import databricks.koalas as ks
-from pyspark.sql import SparkSession
 
 from data.importer import import_data
 from data.splitter import train_valid_test_split_bounds
 
 from preprocessor.features_store import FeatureStore
 
-# TODO(Andrea): should this depend on a command line argument?
-from model.native_xgboost_baseline import Model
-
 from constants import ROOT_DIR
 from util import pretty_evaluation
 
-# Files and paths
-RAW_DATA_INPUT_PATH = {
-    "cluster_complete": "hdfs://BigDataHA/user/s277309/recsys_data/",
-    "cluster_sample_one_hund": "hdfs://BigDataHA/user/s277309/recsys_data_sample/one_hund/",
-    "cluster_sample_200k": "hdfs://BigDataHA/user/s277309/recsys_data_sample/local/sample200k",
-    "local_sampled": "../data/raw/sample_0.0134_noid_notext_notimestamp.parquet",
-    "local_sampled_test": os.path.join(ROOT_DIR, "../data/raw/sample200"),
-    "local_sampled_small": os.path.join(ROOT_DIR, "../data/raw/sample200k"),
-    "test": os.path.join(ROOT_DIR, "../test"),
-}
+PATH_PREPROCESSED = os.path.join(ROOT_DIR, '../data/preprocessed')
 
-PATH_PREPROCESSED = 'data/preprocessed'
-CLUSTER = False # TODO(Francesco): Command line argument? - True to handle multiple partitions on cluster
-
-
-def main(dataset_name):
-
+def main(dataset_path, model_name, is_cluster):
     print("Initializing model...", end = " ")
-    model = Model()
+    model = None
+    if model_name == 'native_xgboost_baseline':
+        from model.native_xgboost_baseline import Model
+        model = Model()
+    if model_name == 'h2o_xgboost_baseline':
+        from model.h2o_xgboost_baseline import Model
+        model = Model()
+    assert model != None, f'cannot find a model with name: {model_name}'
     enabled_features = model.enabled_features
     print("Done")
-    
+
     print("Importing data...", end=" ")
-    raw_data = import_data(path=RAW_DATA_INPUT_PATH[dataset_name])  
+    raw_data = import_data(dataset_path)
     print("Done")
 
     print("Assembling dataset...")
-    store = FeatureStore(PATH_PREPROCESSED, enabled_features, raw_data, CLUSTER)
+    store = FeatureStore(PATH_PREPROCESSED, enabled_features, raw_data, is_cluster)
     features_union_df = store.get_dataset()
     print("Dataset ready")
 
-    
     # Train-valid-test split
     # TODO (manuele): .iloc with arrays is extremely slow on koalas.
     # Slicing works best. We should probably either:
@@ -66,9 +53,11 @@ def main(dataset_name):
     print("Evaluating model")
     print(pretty_evaluation(model.evaluate(test_df)))
 
+arg_parser = argparse.ArgumentParser(description='Process features, train a model, save it, and evaluate it')
+arg_parser.add_argument('dataset_path', type=str, help='path to the full dataset')
+arg_parser.add_argument('model_name', type=str, help='name of the model')
+arg_parser.add_argument('is_cluster', type=bool, help='are we running on the cluster')
+
 if __name__ == "__main__":
-    # run.py [data predefined path or custom path] [model name]
-
-    dataset_name = "local_sampled_test"
-
-    main(dataset_name)
+    args = arg_parser.parse_args()
+    main(args.dataset_path, args.model_name, args.is_cluster)
