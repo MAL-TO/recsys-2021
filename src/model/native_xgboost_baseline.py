0b83b55d7292ddbba2673332964c3cab03bae8cb
Andrea Cognolato <andrecogno@hotmail.it>
2021-05-05 23:06:20 +0200

    Clean up UPD in native_xgboost_baseline.py

--- src/model/native_xgboost_baseline.py
+++ src/model/native_xgboost_baseline.py
@@ -72,14 +72,48 @@ class Model(ModelInterface):
             "eval_metric": "logloss",
         }
         for target in self.target_columns:
-            dtrain = xgb.DMatrix(
-                data=train_df[self.features], label=train_df[target]
-            )
+            dtrain = xgb.DMatrix(data=train_df[self.features], label=train_df[target])
             model = xgb.train(xgb_parameters, dtrain=dtrain)
             model.save_model(self.serialized_model_path_for_target(target))
             self.models[target] = model
 
     def predict(self, features_kdf) -> pd.DataFrame:
+        # Schema for the UDF
+        schema = StructType(
+            [
+                StructField("tweet_id", StringType(), False),
+                StructField("engaging_user_id", StringType(), False),
+                StructField("reply", FloatType(), False),
+                StructField("retweet", FloatType(), False),
+                StructField("retweet_with_comment", FloatType(), False),
+                StructField("like", FloatType(), False),
+            ]
+        )
+
+        # Variables that need to be local so that we don't use `self.` in the UDF
+        # There are pickling + classpath issues with using `self.` inside the UDF since
+        # it needs to be serialized/deserialized when sent to the executors
+        models = {}
+        for target in self.target_columns:
+            model = xgb.Booster()
+            model.load_model(self.serialized_model_path_for_target(target))
+            models[target] = model
+        target_columns = self.target_columns
+        real_features = [
+            "engaged_with_user_follower_count",
+            "engaged_with_user_following_count",
+            "engaging_user_follower_count",
+            "engaging_user_following_count",
+        ]
+
+        @pandas_udf(schema, PandasUDFType.GROUPED_MAP)
+        def predict_udf(features_pdf: pd.DataFrame) -> pd.DataFrame:
+            dfeatures = xgb.DMatrix(data=features_pdf[real_features])
+            for target in target_columns:
+                features_pdf[target] = models[target].predict(dfeatures)
+
+            return features_pdf[["tweet_id", "engaging_user_id"] + target_columns]
+
         with Stage("Converting to DataFrame and caching features_sdf"):
             features_sdf = features_kdf.to_spark(
                 index_col=["tweet_id", "engaging_user_id"]
@@ -87,75 +121,13 @@ class Model(ModelInterface):
             features_sdf.cache()
             del features_kdf
 
-        with Stage("len(features_sdf)"):
-            print("len:", features_sdf.count())
-
-        with Stage("len(features_sdf)"):
-            print("len:", features_sdf.count())
-            print("#partitions: ", features_sdf.rdd.getNumPartitions())
-
-        with Stage("UDF?"):
+        with Stage("Compute predictions"):
             features_with_partition_id_sdf = features_sdf.withColumn(
                 "partition_id", spark_partition_id()
             )
 
-            schema = StructType(
-                [
-                    StructField("tweet_id", StringType(), False),
-                    StructField("engaging_user_id", StringType(), False),
-                    StructField("reply", FloatType(), False),
-                    StructField("retweet", FloatType(), False),
-                    StructField("retweet_with_comment", FloatType(), False),
-                    StructField("like", FloatType(), False),
-                ]
-            )
-
-            # TODO(Andrea): oh god, oh god is this ugly. Fix executor's
-            # pythonpath instead.
-            @pandas_udf(
-                schema,
-                PandasUDFType.GROUPED_MAP,
-            )
-            def predict_udf(features_pdf: pd.DataFrame) -> pd.DataFrame:
-                def serialized_model_path_for_target(target: str) -> str:
-                    p = (
-                        Path(ROOT_DIR)
-                        / "../serialized_models"
-                        / f"native_xgboost_basline_{target}.model"
-                    )
-                    return str(p.resolve())
-
-                models = {}
-                target_columns = [
-                    "reply",
-                    "retweet",
-                    "retweet_with_comment",
-                    "like",
-                ]
-                features = [
-                    "engaged_with_user_follower_count",
-                    "engaged_with_user_following_count",
-                    "engaging_user_follower_count",
-                    "engaging_user_following_count",
-                ]
-                for target in target_columns:
-                    model = xgb.Booster()
-                    model.load_model(serialized_model_path_for_target(target))
-                    models[target] = model
-
-                # TODO(Andrea): why 200 partitions??
-                dfeatures = xgb.DMatrix(data=features_pdf[features])
-                for target in target_columns:
-                    features_pdf[target] = models[target].predict(dfeatures)
-
-                return features_pdf[
-                    ["tweet_id", "engaging_user_id"] + target_columns
-                ]
-
-        # TODO(Andrea): should we use withColumn instead?
-        return features_with_partition_id_sdf.groupBy("partition_id").apply(
-            predict_udf
-        )
+        # TODO(Andrea): why 200 partitions??
+        return features_with_partition_id_sdf.groupBy("partition_id").apply(predict_udf)
 
     def load_pretrained(self):
         for target in self.target_columns:
