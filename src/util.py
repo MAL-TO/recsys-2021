cabf6fc35dd833f1bb545b7c12b788a0308ab13a
Manuele Macchia <macchiamanuele@gmail.com>
2021-05-30 23:04:24 +0200

    Util to remove contents of a directory

--- src/util.py
+++ src/util.py
@@ -1,5 +1,7 @@
 from timeit import default_timer
 import argparse
+import os
+import shutil
 
 
 def pretty_evaluation(results):
@@ -44,3 +46,18 @@ def str2bool(v):
         return False
     else:
         raise argparse.ArgumentTypeError("Boolean value expected.")
+
+
+def rm_dir_contents(path):
+    for filename in os.listdir(path):
+        if filename.startswith("."):
+            continue  # skip dotfiles
+
+        file_path = os.path.join(path, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Error: %s' % (file_path, e))
