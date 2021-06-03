98cb43c4896dc6aebd0e1d8dc4d1d91b9be026bd
Manuele Macchia <macchiamanuele@gmail.com>
2021-06-03 14:44:49 +0200

    Integrate H2O XGBoost (pandas) in our pipeline

--- src/model/h2o_xgboost_baseline.py
+++ src/model/h2o_xgboost_baseline.py
@@ -1,13 +1,22 @@
+from pyspark.sql.types import (
+    FloatType,
+    StringType,
+    StructType,
+    StructField,
+)
+
 import pandas as pd
+import databricks.koalas as ks
 import os
 import contextlib
+from metrics import compute_score
 
 import h2o
 from h2o.estimators import H2OXGBoostEstimator
 
 from model.interface import ModelInterface
 
-# Warning: this class only works with *small*-ish datasets, as it casts Spark
+# WARNING: this class only works with datasets that fit in memory, as it casts Spark
 # dataframes to Pandas dataframes (H2O natively does not work with Spark).
 # Make sure you have enough memory on your machine.
 
@@ -23,7 +32,7 @@ from constants import ROOT_DIR
 
 
 class Model(ModelInterface):
-    def __init__(self, include_targets=True):
+    def __init__(self, include_targets=True, seed=None):
         with open(os.devnull, "w") as devnull:
             with contextlib.redirect_stdout(devnull):
                 h2o.init()
@@ -35,56 +44,60 @@ class Model(ModelInterface):
             raise RuntimeError("H2OXGBoostEstimator is not available!")
 
         self.model = None
-        self.labels = ["reply", "retweet", "retweet_with_comment", "like"]
+        self.seed = seed
+
+        # Specify default and custom features to use in the model
         self.enabled_features = [
-            # Tweet features
-            "tweet_type",
-            "language",
-            "tweet_timestamp",
-            # Engaged-with User (i.e., Engagee) Features
             "engaged_with_user_follower_count",
             "engaged_with_user_following_count",
-            "engaged_with_user_is_verified",
-            "engaged_with_user_account_creation",
-            # Engaging User (i.e., Engager) Features
             "engaging_user_follower_count",
             "engaging_user_following_count",
-            "engaging_user_is_verified",
-            "engaging_user_account_creation",
-            # Engagement features
-            "engagee_follows_engager",
+        ]
+
+        self.labels = [
+            "reply",
+            "retweet",
+            "retweet_with_comment",
+            "like"
+        ]
+
+        # Specify extractors and auxiliaries required by the enabled features
+        self.enabled_auxiliaries = [
+
+        ]
+        self.enabled_extractors = [
+            "engaged_with_user_follower_count",
+            "engaged_with_user_following_count",
+            "engaging_user_follower_count",
+            "engaging_user_following_count",
+            "binarize_timestamps",
         ]
         if include_targets:
-            self.enabled_features += "binarize_timestamps"
+            self.enabled_extractors.append("binarize_timestamps")
 
     @staticmethod
     def serialized_model_path_for_target(target: str) -> str:
         p = (
-            Path(ROOT_DIR)
-            / "../serialized_models"
-            / f"h2o_xgboost_baseline_{target}.model"
+                Path(ROOT_DIR)
+                / "../serialized_models"
+                / f"h2o_xgboost_baseline_{target}.model"
         )
         return str(p.resolve())
 
-    def fit(self, train_data, valid_data, hyperparams):
+    def fit(self, train_data, _valid_data, _hyperparams):
         """Fit model to given training data and validate it.
         Returns the best model found in validation."""
 
         # Cast to h2o frames
         train_frame = h2o.H2OFrame(train_data.to_pandas())
-        valid_frame = h2o.H2OFrame(valid_data.to_pandas())
+        valid_frame = _valid_data
 
-        # TODO: try to implement a hyperparam tuning inside fit (use
-        # additional helper methods in this class if needed, don't
-        # modify the interface).
-        # Try a grid search and/or a random search.
-        # Once a best model has been found, return the best model.
+        # TODO: hyperparameter tuning; unbalancement handling?
 
         models = dict()
         for label in self.labels:
-            # TODO: handle unbalancement (up/down sampling, other?)
             ignored = set(self.labels) - set(label)
-            model = H2OXGBoostEstimator(**hyperparams)
+            model = H2OXGBoostEstimator(seed=self.seed)
             model.train(
                 y=label,
                 ignored_columns=list(ignored),
@@ -101,19 +114,48 @@ class Model(ModelInterface):
 
     def predict(self, test_data):
         """Predict test data. Returns predictions."""
+        schema = StructType(
+            [
+                StructField("reply", FloatType(), False),
+                StructField("retweet", FloatType(), False),
+                StructField("retweet_with_comment", FloatType(), False),
+                StructField("like", FloatType(), False),
+                StructField("tweet_id", StringType(), False),
+                StructField("engaging_user_id", StringType(), False),
+            ]
+        )
 
-        test_frame = h2o.H2OFrame(test_data.to_pandas())
+        # DataFrame.to_pandas() drops the index, so we need to save it
+        # separately and reattach it later.
 
-        predictions = pd.DataFrame()
+        # H2OFrame does not provide an index like pandas, but rather appears
+        # to have an internal numerical index to preserve ordering.
+        # https://docs.h2o.ai/h2o/latest-stable/h2o-py/docs/frame.html#h2oframe
+
+        # So we trust that H2O keeps everything in order and drop our custom
+        # index ["tweet_id", "engaging_user_id"] in favour of a "standard"
+        # numerical index such as 0, 1, 2, ..., only to reattach it later
+        # when returning the predictions DataFrame.
+
+        ks_test_data_index = test_data.reset_index(drop=False)
+        ks_index = ks_test_data_index[["tweet_id", "engaging_user_id"]]
+
+        h2oframe_test = h2o.H2OFrame(test_data.to_pandas())
+
+        df_predictions = pd.DataFrame()
         for label in self.labels:
-            predictions[label] = (
+            df_predictions[label] = (
                 self.model[label]
-                .predict(test_frame)
-                .as_data_frame()["True"]
-                .values
+                    .predict(h2oframe_test)
+                    .as_data_frame()["True"]
+                    .values
             )
 
-        return predictions
+        # Reattach real index (Lord have mercy)
+        df_predictions = df_predictions.join(ks_index.to_pandas())
+        ks_predictions = ks.DataFrame(df_predictions)
+
+        return ks_predictions.to_spark()
 
     def load_pretrained(self):
         self.model = {}
