494833592a25c96ac5d0a0a374d0f00cb64e06ce
Manuele Macchia <macchiamanuele@gmail.com>
2021-06-03 15:09:23 +0200

    Move constants and paths to `constants.py`

--- src/constants.py
+++ src/constants.py
@@ -1,2 +1,19 @@
 import os
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))  # local
+ROOT_DIR_CLUSTER = "file:///home/bigdata-01QYD/sXXXXXX/recsys-2021"
+
+PATH_PREPROCESSED = os.path.join(ROOT_DIR, "../data/preprocessed")
+PATH_PREPROCESSED_CLUSTER = os.path.join(ROOT_DIR_CLUSTER, "data/preprocessed")
+
+PATH_AUXILIARIES = os.path.join(ROOT_DIR, "../data/auxiliary")
+PATH_AUXILIARIES_CLUSTER = os.path.join(ROOT_DIR_CLUSTER, "data/auxiliary")
+
+MODEL_SEED = 42
+
+# List of Python dicts, each dict represents a fold with train and test set
+# filenames inside `PATH_DATA`
+PATH_DATA = "../data/raw"
+FILENAMES_DATA = [
+    {"train": "", "test": ""},
+]
